65b53133838667b9e9b7f02bd71a76f58c0e65c2
rohanpatankar <rohanpatankar926@gmail.com>
2022-11-05 13:31:35 +0530

    hydra connection

--- mongodb.py
+++ mongodb.py
@@ -2,23 +2,41 @@ import pymongo
 from pymongo import MongoClient
 from read_yaml import ReadYaml
 import json
+import hydra
+from omegaconf import DictConfig
 
-class MongoDb:
-        def __init__(self,config_path:str="config.yaml" ):
-                self.config=ReadYaml().read_params(config_path)
-        
-        def load_data(self):
+# class MongoDb:
+#         def __init__(self):
+#             pass
+
+#         @hydra.main(config_path="config", config_name="config")
+#         def load_data(config: DictConfig):
+#                 try:
+#                     self.myClient=pymongo.MongoClient(config.mongoDbCredentials.uri)
+#                     self.database=self.myClient[config.mongoDbCredentials.database]
+#                     self.col=self.database[config.mongoDbCredentials.collection]
+#                     with open(config.Data.json_data) as json_data:
+#                         json_data_=json.loads(json_data.read())
+#                     self.DummyData = json_data_ 
+#                     self.load_data = self.col.insert_one(self.DummyData)
+#                     return f"Data Loaded successfully at {self.load_data}"
+#                 except Exception as e:
+#                     raise e
+
+
+@hydra.main(config_path="config", config_name="config")
+def load_data(config: DictConfig):
                 try:
-                    self.myClient=pymongo.MongoClient(self.config["mongoDbCredentials"]["uri"])
-                    self.database=self.myClient[self.config["mongoDbCredentials"]["database"]]
-                    self.col=self.database[self.config["mongoDbCredentials"]["collection"]]
-                    with open(self.config["Data"]["json_data"]) as json_data:
+                    myClient=pymongo.MongoClient(config.mongoDbCredentials.uri)
+                    database=myClient[config.mongoDbCredentials.database]
+                    col=database[config.mongoDbCredentials.collection]
+                    with open(config.Data.json_data) as json_data:
                         json_data_=json.loads(json_data.read())
-                    self.DummyData = json_data_ 
-                    self.load_data = self.col.insert_one(self.DummyData)
-                    return f"Data Loaded successfully at {self.load_data}"
+                    DummyData = json_data_ 
+                    load_data = col.insert_one(DummyData)
+                    return f"Data Loaded successfully at {load_data}"
                 except Exception as e:
                     raise e
 
 if __name__=="__main__":
-        MongoDb().load_data()
+        load_data()
